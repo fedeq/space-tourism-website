e9455c32824e0e3ed68a1730afcd91bb43c26324
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2024-01-03 18:18:55 -0300

    Show active page in header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import {Header} from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="xl:bg-home_desktop bg-home_mobile sm:bg-home_tablet grid min-h-screen grid-rows-[auto,1fr] bg-cover bg-center bg-no-repeat">
+    <div className="grid min-h-screen grid-rows-[auto,1fr] bg-home_mobile bg-cover bg-center bg-no-repeat sm:bg-home_tablet xl:bg-home_desktop">
       <Header />
       <main className="p-4">
         <div className="container mx-auto flex h-full flex-col items-center justify-evenly px-6 md:px-0 md:py-20 xl:flex-row xl:items-end ">
@@ -13,17 +13,15 @@ export default function Home() {
             <div className="font-bellefair text-[80px] font-normal text-white md:text-[150px]">
               SPACE
             </div>
-            <div className="font-barlow max-w-[444px] text-sm font-normal leading-7 text-indigo-200 md:text-base md:text-lg">
+            <div className="max-w-[444px] font-barlow text-sm font-normal leading-7 text-indigo-200 md:text-base xl:text-lg">
               Let’s face it; if you want to go to space, you might as well genuinely go to outer
               space and not hover kind of on the edge of it. Well sit back, and relax because we’ll
               give you a truly out of this world experience!
             </div>
           </div>
           <div className="mt-16 flex justify-center md:w-1/2">
-            <div className="flex h-[150px] w-[150px] items-center justify-center rounded-full bg-white hover:cursor-pointer hover:ring-[50px] hover:ring-white hover:ring-opacity-20 md:h-[274px] md:w-[274px]">
-              <div className="font-bellefair text-xl font-normal tracking-widest text-gray-950 md:text-[32px]">
-                EXPLORE
-              </div>
+            <div className="flex h-[150px] w-[150px] items-center justify-center rounded-full bg-white font-bellefair text-xl font-normal tracking-widest text-gray-950 hover:cursor-pointer hover:ring-[50px] hover:ring-white hover:ring-opacity-20 md:h-[274px] md:w-[274px] md:text-[32px]">
+              EXPLORE
             </div>
           </div>
         </div>
--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,17 +1,33 @@
 "use client";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 import {navLinks} from "./Header";
 
 export function NavLinks() {
+  // get current url
+  const pathname = usePathname();
+
   return navLinks.map(({href, number, text}) => (
-    <NavLink key={href} href={href} number={number} text={text} />
+    <NavLink key={href} href={href} isActive={pathname == href} number={number} text={text} />
   ));
 }
-function NavLink({href, number, text}: {href: string; number: string; text: string}) {
+function NavLink({
+  href,
+  number,
+  text,
+  isActive,
+}: {
+  href: string;
+  number: string;
+  text: string;
+  isActive: boolean;
+}) {
   return (
     <Link
-      className="flex items-center border-transparent hover:border-white hover:border-opacity-50 active:border-white active:border-opacity-100"
+      className={`flex items-center border-transparent hover:border-white hover:border-opacity-50 ${
+        isActive ? "border-white border-opacity-100" : ""
+      }`}
       href={href}
     >
       <div className="flex gap-2">
