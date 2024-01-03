be65cb0359f15cc48fcb8f92fb36e7d2bf406251
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2024-01-03 17:21:53 -0300

    Fix some accessiblity issues

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import {useState} from "react";
-import { useSwipeable } from "react-swipeable"
+import {useSwipeable} from "react-swipeable";
 
 import {Header} from "@/components/Header";
 
@@ -37,8 +37,10 @@ const crew = [
 export default function Crew() {
   const [crewMember, setCrewMember] = useState(0);
   const handlers = useSwipeable({
-    onSwipedRight: (eventData) => setCrewMember((currentCrewMemeber) => (currentCrewMemeber - 1 + crew.length) % crew.length),
-    onSwipedLeft: (eventData) => setCrewMember((currentCrewMemeber) => (currentCrewMemeber + 1) % crew.length),
+    onSwipedRight: (eventData) =>
+      setCrewMember((currentCrewMemeber) => (currentCrewMemeber - 1 + crew.length) % crew.length),
+    onSwipedLeft: (eventData) =>
+      setCrewMember((currentCrewMemeber) => (currentCrewMemeber + 1) % crew.length),
   });
 
   const crewMemberSelector = (
@@ -47,6 +49,7 @@ export default function Crew() {
         className={`h-2.5 w-2.5 rounded-full xl:h-[15px] xl:w-[15px] ${
           crewMember == 0 ? "bg-white" : "bg-indigo-200"
         }`}
+        name="Crew Member 1"
         type="button"
         onClick={() => setCrewMember(0)}
       />
@@ -54,6 +57,7 @@ export default function Crew() {
         className={`h-2.5 w-2.5 rounded-full xl:h-[15px] xl:w-[15px] ${
           crewMember == 1 ? "bg-white" : "bg-indigo-200"
         }`}
+        name="Crew Member 2"
         type="button"
         onClick={() => setCrewMember(1)}
       />
@@ -61,6 +65,7 @@ export default function Crew() {
         className={`h-2.5 w-2.5 rounded-full xl:h-[15px] xl:w-[15px] ${
           crewMember == 2 ? "bg-white" : "bg-indigo-200"
         }`}
+        name="Crew Member 3"
         type="button"
         onClick={() => setCrewMember(2)}
       />
@@ -68,6 +73,7 @@ export default function Crew() {
         className={`h-2.5 w-2.5 rounded-full xl:h-[15px] xl:w-[15px] ${
           crewMember == 3 ? "bg-white" : "bg-indigo-200"
         }`}
+        name="Crew Member 4"
         type="button"
         onClick={() => setCrewMember(3)}
       />
@@ -75,7 +81,7 @@ export default function Crew() {
   );
 
   return (
-    <div className="xl:bg-crew_desktop bg-crew_mobile sm:bg-crew_tablet grid min-h-screen grid-rows-[auto,1fr] bg-cover bg-center bg-no-repeat">
+    <div className="grid min-h-screen grid-rows-[auto,1fr] bg-crew_mobile bg-cover bg-center bg-no-repeat sm:bg-crew_tablet xl:bg-crew_desktop">
       <Header />
       <main {...handlers}>
         <div className="grid h-full grid-cols-1 gap-10 xl:grid-cols-2 xl:gap-0">
@@ -84,29 +90,38 @@ export default function Crew() {
               <span className="font-barlow_condensed font-bold tracking-[4.72px] text-white opacity-25">
                 02
               </span>
-              <span className="font-barlow_condensed ml-6 font-normal uppercase tracking-[4.72px] text-white">
+              <span className="ml-6 font-barlow_condensed font-normal uppercase tracking-[4.72px] text-white">
                 Meet your crew
               </span>
             </div>
             <div className="mt-8 flex h-[40%] w-[90%] items-end justify-center border-b border-b-gray-700 md:hidden">
-              <img alt="Crew" className="mt-10 px-20 md:px-0 animate-fade" src={crew[crewMember].image} />
+              <img
+                alt="Crew"
+                className="mt-10 animate-fade px-20 md:px-0"
+                src={crew[crewMember].image}
+              />
             </div>
             <div className="flex gap-6 py-5 md:hidden">{crewMemberSelector}</div>
             <div className="mt-4 flex w-full flex-col items-center justify-center md:mt-14 md:h-[270px] xl:mt-0 xl:h-full xl:items-start">
               <div className="font-bellefair text-base font-normal uppercase text-white opacity-50 md:text-2xl xl:text-[32px]">
                 {crew[crewMember].role}
               </div>
-              <div className="font-bellefair mt-2 inline-block text-2xl font-normal uppercase text-white md:mt-4 md:text-[40px] xl:mt-8 xl:text-[56px]">
+              <div className="mt-2 inline-block font-bellefair text-2xl font-normal uppercase text-white md:mt-4 md:text-[40px] xl:mt-8 xl:text-[56px]">
                 {crew[crewMember].name}
               </div>
-              <p className="font-barlow mt-4 px-4 text-center text-sm font-thin leading-loose text-indigo-200 md:mt-8 md:w-[444px] md:px-0 md:text-base xl:mt-12 xl:text-left xl:text-lg">
+              <p className="mt-4 px-4 text-center font-barlow text-sm font-thin leading-loose text-indigo-200 md:mt-8 md:w-[444px] md:px-0 md:text-base xl:mt-12 xl:text-left xl:text-lg">
                 {crew[crewMember].info}
               </p>
               <div className="hidden md:mt-12 md:flex md:gap-6 xl:mt-20">{crewMemberSelector}</div>
             </div>
           </div>
           <div className="hidden justify-center md:flex">
-            <img key={crew[crewMember].name} alt="Crew" className="mt-10 px-20 md:px-0 animate-fade" src={crew[crewMember].image} />
+            <img
+              key={crew[crewMember].name}
+              alt="Crew"
+              className="mt-10 animate-fade px-20 md:px-0"
+              src={crew[crewMember].image}
+            />
           </div>
         </div>
       </main>
