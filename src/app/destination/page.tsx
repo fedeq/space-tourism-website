81a316ec3dbf8b6cd614a194dbbed366cf563d46
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2024-01-03 18:02:00 -0300

    Now!

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,49 +1,6 @@
 import {Header} from "@/components/Header";
 import {DestinationTabs} from "@/components/DestinationTabs";
 
-type Destination = "moon" | "europa" | "titan" | "mars";
-
-interface DestinationTab {
-  destinationName: string;
-  image: string;
-  details: string;
-  avgDistance: string;
-  estTravelTime: string;
-}
-
-export const tabs: DestinationTab[] = [
-  {
-    destinationName: "Mars",
-    image: "/destination/image-mars.webp",
-    details: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the
-    tallest planetary mountain in our solar system. It’s two and a half times the size of
-    Everest!`,
-    avgDistance: "225 MIL. km",
-    estTravelTime: "9 MONTHS",
-  },
-  {
-    destinationName: "Europa",
-    image: "/destination/image-europa.webp",
-    details: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.`,
-    avgDistance: "628 MIL. km",
-    estTravelTime: "3 years",
-  },
-  {
-    destinationName: "Titan",
-    image: "/destination/image-titan.webp",
-    details: `The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.`,
-    avgDistance: "1.6 BIL. km",
-    estTravelTime: "7 years",
-  },
-  {
-    destinationName: "Moon",
-    image: "/destination/image-moon.webp",
-    details: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.`,
-    avgDistance: "384,400 km",
-    estTravelTime: "3 days",
-  },
-];
-
 export default function Destination() {
   return (
     <div className="grid min-h-screen grid-rows-[auto,1fr] bg-destination_mobile bg-cover bg-center bg-no-repeat sm:bg-destination_tablet xl:bg-destination_desktop">
--- src/components/DestinationTabs.tsx
+++ src/components/DestinationTabs.tsx
@@ -3,7 +3,46 @@ import Image from "next/image";
 import {useState} from "react";
 import {useSwipeable} from "react-swipeable";
 
-import {tabs} from "../app/destination/page";
+interface DestinationTab {
+  destinationName: string;
+  image: string;
+  details: string;
+  avgDistance: string;
+  estTravelTime: string;
+}
+
+const tabs: DestinationTab[] = [
+  {
+    destinationName: "Mars",
+    image: "/destination/image-mars.webp",
+    details: `Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the
+    tallest planetary mountain in our solar system. It’s two and a half times the size of
+    Everest!`,
+    avgDistance: "225 MIL. km",
+    estTravelTime: "9 MONTHS",
+  },
+  {
+    destinationName: "Europa",
+    image: "/destination/image-europa.webp",
+    details: `The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.`,
+    avgDistance: "628 MIL. km",
+    estTravelTime: "3 years",
+  },
+  {
+    destinationName: "Titan",
+    image: "/destination/image-titan.webp",
+    details: `The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.`,
+    avgDistance: "1.6 BIL. km",
+    estTravelTime: "7 years",
+  },
+  {
+    destinationName: "Moon",
+    image: "/destination/image-moon.webp",
+    details: `See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.`,
+    avgDistance: "384,400 km",
+    estTravelTime: "3 days",
+  },
+];
 
 export function DestinationTabs() {
   const [destination, setDestination] = useState(0);
